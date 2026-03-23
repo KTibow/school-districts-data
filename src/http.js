eaa086475e16a3526cc023b7d2c3495d53a1bf3f
KTibow <KTibow@users.noreply.github.com>
2026-03-23 16:19:02 -0700

    actually throw errors

--- src/http.js
+++ src/http.js
@@ -1,9 +1,9 @@
 const USER_AGENT =
-  'school-districts-data/0.1 (+https://github.com/KTibow/school-districts-data)';
+  "school-districts-data/0.1 (+https://github.com/KTibow/school-districts-data)";
 
 export const fetchResponse = async (url, init = {}) => {
   const headers = new Headers(init.headers);
-  headers.set('user-agent', USER_AGENT);
+  headers.set("user-agent", USER_AGENT);
 
   return await fetch(url, {
     ...init,
@@ -13,7 +13,11 @@ export const fetchResponse = async (url, init = {}) => {
 
 export const fetchText = async (url, init = {}) => {
   const response = await fetchResponse(url, init);
+  if (!response.ok) {
+    throw new Error(`${response.statusText}`);
+  }
   return await response.text();
 };
 
-export const fetchJson = async (url, init = {}) => JSON.parse(await fetchText(url, init));
+export const fetchJson = async (url, init = {}) =>
+  JSON.parse(await fetchText(url, init));
